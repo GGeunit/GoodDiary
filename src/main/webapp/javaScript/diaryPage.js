d91cda23708fe2a7ead722984e720f17f81377be
zeew00kim <tukzeew00@gmail.com>
2024-11-30 22:11:36 +0900

    js_func_2

--- src/main/webapp/javaScript/diaryPage.js
+++ src/main/webapp/javaScript/diaryPage.js
@@ -3,13 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     
     if (cancelButton) {
         cancelButton.addEventListener("click", (event) => {
-            // 폼 리셋 방지
             event.preventDefault();
-
-            // 확인창 띄우기
             const userConfirmed = confirm("이전 페이지로 돌아가시겠습니까?");
             if (userConfirmed) {
-                // 페이지 이동
                 window.location.href = "diaryListPage.jsp";
             }
         });
